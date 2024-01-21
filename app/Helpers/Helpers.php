0e335e22ed7b430ae15f5ab6ff4a598026aaf855
ballack <rodrigue-dev>
2024-01-21 06:59:28 +0100

    ert

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -7,7 +7,21 @@ use Illuminate\Support\Str;
 
 class Helpers
 {
-    public static function uploadPDF(string $format, $image = null,string $name)
+    public static function uploadImage($data,$type,$dest){
+        $image_parts = explode(";base64,", $data);
+        $image_base64 = base64_decode($image_parts[1]);
+        if ($type === 'image/png') {
+            $imagename = uniqid() . '.png';
+        } else {
+            $imagename = uniqid() . '.jpg';
+        }
+        if (!Storage::disk('public')->exists($dest)) {
+            Storage::disk('public')->makeDirectory($dest);
+        }
+        Storage::disk('public')->put($dest.'/' . $imagename, $image_base64);
+        return env("APP_URL").'/storage/'.$dest."/".$imagename;
+    }
+    public static function uploadPDF($format, $image = null,string $name)
     {
         if ($image != null) {
             if (!Storage::disk('public')->exists("cvs")) {
--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,9 +3,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Helpers;
 use App\Http\Resources\BlogResource;
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use function Symfony\Component\String\Slugger\slug;
 
 class BlogController extends Controller
 {
@@ -18,41 +20,46 @@ class BlogController extends Controller
     public function create()
     {
         //
-        
+
     }
 
     public function store(Request $request)
     {
-        //
-        $blog = new Blog();
+
+        if(is_null($request->id)){
+            $blog = new Blog();
+            $blog->created_by       = $request->created_by;
+        }else{
+            $blog=Blog::find($request->id);
+        }
+        if (!is_null($request->featured_image)){
+            $image_url=Helpers::uploadImage($request->featured_image,$request->featured_type,"blogs");
+            $blog->cannonical_link  = $image_url;
+            $blog->featured_image   = $image_url;
+        }
 
         $blog->name             = $request->name;
-        $blog->slug             = $request->slug;
+        $blog->slug             = $request->name;
         $blog->publish_datetime = $request->publish_datetime;
         $blog->content          = $request->content;
         $blog->meta_title       = $request->meta_title;
-        $blog->cannonical_link  = $request->cannonical_link;
         $blog->meta_keywords    = $request->meta_keywords;
-        $blog->status           = $request->status;
-        $blog->featured_image   = $request->featured_image;
-        $blog->created_by       = $request->created_by;
+        $blog->status           = $request->status; /// 1 or 0
         $blog->updated_by       = $request->updated_by;
-
         $blog->save();
 
         return response()->json('Le blog a été ajoutée avec succès !');
     }
- 
+
     public function show(Blog $blog)
     {
-        //
         return new BlogResource($blog) ;
     }
 
     public function edit(Request $request)
     {
         //
-        
+
     }
 
     public function update(Request $request, $id)
@@ -62,7 +69,7 @@ class BlogController extends Controller
         $blog->update($request->all());
 
         return response()->json($blog, 200);
-        
+
     }
 
     public function destroy(Blog $blog)
